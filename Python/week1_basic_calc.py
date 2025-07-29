f3d6bd59bbe786c946dae075c94994a0fba46c91
theespeedninja <dannygitonga1@gmail.com>
2025-07-29 22:58:22 +0300

    Basic Calc edit

--- Python/week1_basic_calc.py
+++ Python/week1_basic_calc.py
@@ -1,18 +1,20 @@
 print("🧮BASIC CALCULATOR🧮")
+#asking for user input for the two numbres
 num_1 = int( input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 operrand = input("Enter operator (➕ , ➖ , ✖️ (*) , ➗(/) ): ")
+#Checking the operator and performing the calculation
 if operrand == "+":
     result = num_1 + num_2
-    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}")
+    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}") #Addition ➕
 elif operrand == "-":
     result = num_1 - num_2
-    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}")
+    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}") #Subtraction ➖
 elif operrand == "*":
     result = num_1 * num_2
-    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}")
+    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}") #Multiplication ✖️
 elif operrand == "/":
     result =float( num_1 / num_2)
-    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}")
+    print(f"🚀🚀🟰\n{num_1} {operrand} {num_2} = {result}") #Divison ➗
 else:
-    print("\n‼️‼️‼️Invalid operator. Please use (➕ , ➖ , ✖️ (*) , ➗(/) ).")
+    print("\n‼️‼️‼️Invalid operator. Please run the code again and make sure to use (➕ , ➖ , ✖️ (*) , ➗(/) ).")
